ae8ea1dd2ea4719a31a5d50b41b0e55ab3714eed
paranoidninja <paranoidninja@protonmail.com>
2018-05-06 16:47:35 +0530

    modified:   part_2-2/crackMe_xor.cpp 	modified:   part_2-2/crackMe_xor.exe

--- part_2-2/crackMe_xor.cpp
+++ part_2-2/crackMe_xor.cpp
@@ -21,21 +21,27 @@ void CheckPass(int *XoredPassword) {
     }
 }
 
-int main() {
-    char toEncrypt[10] = "";
+int main(int argc, char *argv[]) {
     
-    printf("Enter the 10 character password: ");
-    scanf("%10s", toEncrypt);
-    int stringLength = strlen(toEncrypt);
-
-    int keyStore[10] = {85, 86, 87, 88, 89, 90, 81, 82, 83, 84};
-    int XoredDecimal[stringLength] = {};
-
-    for (int i=0; i<stringLength; i++) {
-        XoredDecimal[i] = ((int)(toEncrypt[i]))^(keyStore[i]);
+    if (argc != 2) {
+        printf("Help:\n%s <password 10 chars max>\n", argv[0]);
     }
-    
-    CheckPass(XoredDecimal);
-    
+    else {
+    //     char toEncrypt[10] = argv[1];
+        int stringLength = strlen(argv[1]);
+        if (stringLength > 10) {
+            printf("[-] Password must be a maximum of 10 characters. Please enter a shorter password\n");
+        }
+        else {
+            int keyStore[10] = {85, 86, 87, 88, 89, 90, 81, 82, 83, 84};
+            int XoredDecimal[stringLength] = {};
+
+            for (int i=0; i<stringLength; i++) {
+                XoredDecimal[i] = ((int)(argv[1][i]))^(keyStore[i]);
+            }
+            
+            CheckPass(XoredDecimal);
+        }
+    }
     return 0;
 }
